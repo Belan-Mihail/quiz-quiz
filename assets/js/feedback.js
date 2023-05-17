2e26f2c0dc3c40079614d96327bc6142f81ad6c1
Belan-Mihail Belan <belan_mihail@inbox.ru>
2023-05-17 21:06:41 +0000

    renaming the contact.js page to feedback.js. Adding comments to the code on the feedback.js page

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -5,17 +5,20 @@
 */
 
 function sendMail() {
+    //prevent standart form action
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
     });
 
+    //binding form elements to properties emailjs
     var params = {
         from_name : document.getElementById('yname').value,
         email_id : document.getElementById('email_id').value,
         message : document.getElementById('message').value
-    }
+    };
+    //function for sending users messages from feedback form
     emailjs.send('service_atmswmr', 'template_vlu0u11', params).then(function (res) {
         alert("Success!" + res.status);
-    })
+    });
 }
 
